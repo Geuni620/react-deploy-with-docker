cef47fc5e71ff22559110a4f58886581f9aa6731
Geuni <dlrmsgnl620@naver.com>
2023-06-06 23:14:33 +0900

    Add: Multi-Stage Build for Production with NGINX

--- src/App.js
+++ src/App.js
@@ -10,7 +10,6 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>{`hello ${process.env.REACT_APP_NAME}`}</p>
-        <p>{`hello ${process.env.REACT_APP_TITLE}`}</p>
         <a
           className="App-link"
           href="https://reactjs.org"
